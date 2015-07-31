3f79c902e0110b82337d1d1a3c41430c3a8c5e10
Pablo Vallejo <pabloninety@gmail.com>
2015-07-31 14:28:51 -0500

    Build: improve gulp file workflow

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,34 @@
 //
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
 
 
 // Compile sass.
 gulp.task('sass', function () {
   gulp.src('./assets/sass/*.sass')
     .pipe(sass().on('error', sass.logError))
+    .pipe(concat('styles.min.css'))
+    .pipe(minifyCss())
     .pipe(gulp.dest('./assets/css'));
 });
 
-// Move necessary bower component files into assets.
-gulp.task('move_assets', function() {
+// Move necessary bower components files into assets.
+gulp.task('libraries', function() {
 
-    // Styles.
+    // Libraries.
     gulp.src([
         'bower_components/bootstrap/dist/css/bootstrap.min.css',
         'bower_components/jquery-github/assets/base.css',
+    ])
+    .pipe(concat('libs.min.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('assets/css/libs'));
+
+    // Fonts.
+    gulp.src([
         'bower_components/jquery-github/assets/*.{ttf,woff,eof,svg}'
     ])
     .pipe(gulp.dest('assets/css/libs'));
@@ -28,9 +40,16 @@ gulp.task('move_assets', function() {
         'bower_components/bootstrap/dist/js/bootstrap.min.js',
         'bower_components/jquery-github/dist/jquery.github.min.js'
     ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('assets/js/libs'));
 });
 
-gulp.task('sass:watch', function () {
+// Watch .sass files for changes.
+gulp.task('watch', function () {
   gulp.watch('./assets/sass/*.sass', ['sass']);
 });
+
+
+// Default task.
+gulp.task('default', ['sass', 'libraries', 'watch']);
